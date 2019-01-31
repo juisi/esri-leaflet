c5947302ed919db36ec411f23f5bf5684795584f
jussikarvonen <jussi.karvonen@distence.fi>
2019-01-31 16:15:27 +0200

    modify custom geojson static geometry

--- src/Layers/FeatureLayer/FeatureLayer.js
+++ src/Layers/FeatureLayer/FeatureLayer.js
@@ -35,7 +35,7 @@ export var FeatureLayer = FeatureManager.extend({
   },
 
   createNewLayer: function (geojson) {
-    if (!geojson.geometry.type) {
+    if (geojson.geometry.type === null) {
       console.log('Esri-FeatureLayer: missing geojson geometry object ');
       geojson.geometry = {'type': 'Polygon', 'coordinates': [[[31.465492, 62.975368], [31.56863, 62.918721], [31.516295, 62.977954], [31.465492, 62.975368]]]};
       console.log('Esri-FeatureLayer:add static values: ', geojson.geometry);
